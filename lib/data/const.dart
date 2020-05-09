c8201d14d07518682678d9119f3c5cab4789cbae
zzy <zhangzhanyong@julive.com>
2020-05-09 17:51:02 +0800

    【add about me】

--- lib/data/const.dart
+++ lib/data/const.dart
@@ -1,11 +1,59 @@
+import 'beans.dart';
 
 /// config ---------------------------------------------------------------------------------------
-const String CHAT_GROUP_TEXT = "欢迎光临，进群就有红包领哦。加上面微信号，备注Flutter Web，加好友后拉群，一起交流学习。";
+const String CHAT_GROUP_TEXT =
+    "欢迎光临，进群就有红包领哦。加上面微信号，备注Flutter Web，加好友后拉群，一起交流学习。";
 
+const String about = 'Call';
+const String me = ' Me';
+const String headline = '我是i校长,至今从事Android开发6年,目前就职于居理新房,为满足人类的一切居住理想而努力';
+const String skills_i_have = '技能';
+const String experience = '经验';
+final List<Education> educationList = [
+  Education(
+    '10 2018',
+    '-至今',
+    '居理新房',
+    'Android System Engineer',
+  ),
+  Education(
+    '9 2017',
+    '-10 2018',
+    '车多多',
+    'Android Software Engineer',
+  ),
+  Education(
+    '2 2014',
+    '-5 2016',
+    '中和农信',
+    'Android Software Engineer',
+  ),
+];
 
-
-
-
+const skills = [
+  'Java',
+  'Kotlin',
+  'Dart',
+  'Flutter',
+  'Android',
+  'React',
+  'Jenkins',
+  'React Native',
+  '小程序',
+  'Scrum Master',
+  'Js',
+  'NodeJs',
+  'Docker',
+  'Python',
+  'JVM',
+  'Git',
+  'JIRA',
+];
 
 /// images ---------------------------------------------------------------------------------------
 const String WE_CHAT_SCAN = "images/wechat.jpeg";
+const String programmer3 = 'images/programer.gif';
+ const String qq = 'images/tx.png';
+ const String jianshu = 'images/jianshu.png';
+ const String gmail = 'images/gmail.png';
+ const String weixin = 'images/weixin.png';
--- lib/pages/menu_about.dart
+++ lib/pages/menu_about.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:jetpack/data/beans.dart';
+import 'package:jetpack/data/const.dart';
+import 'package:jetpack/widgets/responsive_widget.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class WidgetMenuAbout extends StatefulWidget {
   @override
@@ -8,46 +13,239 @@ class WidgetMenuAbout extends StatefulWidget {
 class _WidgetMenuAboutState extends State<WidgetMenuAbout> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: GridTile(
-        header: Container(
-          color: Color(0x22000000),
-          height: 50,
-          child: Text(
-            'header',
-            style: TextStyle(color: Colors.redAccent),
-          ),
-        ),
-        child: Container(
-          color: Color(0x22119233),
-          height: 50,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                'start',
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.redAccent),
-              ),
-              Text(
-                'end',
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.redAccent),
-              ),
-            ],
-          ),
-        ),
-        footer: Container(
-//          color: Color(0x44000000),
-          color: Color(0x55000000),
-          height: 50,
-          child: Text(
-            'footer',
-            style: TextStyle(color: Colors.redAccent),
-          ),
-        ),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: _buildLargeScreen(),
       ),
     );
   }
+
+  _buildLargeScreen() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        _buildIllustration(),
+        SizedBox(
+          height: 8,
+        ),
+        _buildhomeMe(),
+        SizedBox(
+          height: 8,
+        ),
+        _buildSocialIcons(),
+        SizedBox(
+          height: 8,
+        ),
+        _buildHeadline(),
+        SizedBox(
+          height: 8,
+        ),
+        _buildEducation(),
+        SizedBox(
+          height: 8,
+        ),
+        _buildSkills(),
+
+      ],
+    );
+  }
+
+  Widget _buildIllustration() {
+    return Align(
+      alignment: Alignment.center,
+      child: Image.asset(
+        programmer3,
+        height: 360,
+      ),
+    );
+  }
+
+  Widget _buildEducation() {
+    return Column(
+      children: <Widget>[
+        _buildEducationContainerHeading(),
+        SizedBox(height: 8.0),
+        _buildEducationSummary(),
+        SizedBox(height: 8.0),
+        _buildEducationTimeline(),
+      ],
+    );
+  }
+
+  Widget _buildEducationContainerHeading() {
+    return Text(experience, style: Theme.of(context).textTheme.subtitle1);
+  }
+
+  Widget _buildEducationSummary() {
+    return Text('性能的关键是精简，而不是一堆的优化用例。除非有真正显著的效果，否则一定要忍住你那些蠢蠢欲动的小微调的企图。',
+        style: Theme.of(context).textTheme.subtitle2);
+  }
+
+  Widget _buildEducationTimeline() {
+    final List<Widget> widgets = educationList
+        .map((education) => _buildEducationTile(education))
+        .toList();
+    return Column(children: widgets);
+  }
+
+  Widget _buildEducationTile(Education education) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        children: <Widget>[
+          Text('${education.post}',
+              style: Theme.of(context).textTheme.bodyText2),
+          Text('${education.organization}',
+              style: Theme.of(context).textTheme.bodyText2),
+          Text('${education.from}-${education.to}',
+              style: Theme.of(context).textTheme.bodyText2),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSkills() {
+    final List<Widget> widgets = skills
+        .map((skill) => Padding(
+              padding: EdgeInsets.only(right: 16.0),
+              child: _buildSkillChip(context, skill),
+            ))
+        .toList();
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        _buildSkillsContainerHeading(),
+        ResponsiveWidget.isLargeScreen(context)
+            ? Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Container(),
+                  ),
+                  Expanded(flex: 4, child: Wrap(children: widgets)),
+                  Expanded(
+                    flex: 1,
+                    child: Container(),
+                  )
+                ],
+              )
+            : Wrap(children: widgets),
+      ],
+    );
+  }
+
+  Widget _buildSkillsContainerHeading() {
+    return Text(skills_i_have, style: Theme.of(context).textTheme.subtitle1);
+  }
+
+  Widget _buildSkillChip(BuildContext context, String label) {
+    return Chip(
+      label: Text(label, style: Theme.of(context).textTheme.subtitle2),
+    );
+  }
+
+  Widget _buildhomeMe() {
+    return RichText(
+      text: TextSpan(
+        // Note: Styles for TextSpans must be explicitly defined.
+        // Child text spans will inherit styles from parent
+        children: <TextSpan>[
+          TextSpan(text: about, style: Theme.of(context).textTheme.subtitle1),
+          TextSpan(text: me, style: Theme.of(context).textTheme.subtitle2),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHeadline() {
+    return Text(
+      headline,
+      style: Theme.of(context).textTheme.subtitle2,
+    );
+  }
+
+  Widget _buildSocialIcons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {
+            showDialog(
+                context: context,
+                child: AlertDialog(
+                  title: Text("微信号"),
+                  content: Text("zhanyong0425"),
+                  actions: <Widget>[
+                    FlatButton(
+                        onPressed: () {
+                          Clipboard.setData(
+                              ClipboardData(text: 'zhanyong0425'));
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('复制')),
+                  ],
+                ));
+          },
+          child: Image.asset(
+            weixin,
+            height: 20.0,
+            width: 20.0,
+          ),
+        ),
+        SizedBox(width: 16.0),
+        GestureDetector(
+          onTap: () {
+            showDialog(
+                context: context,
+                child: AlertDialog(
+                  title: Text("QQ"),
+                  content: Text("1608889789"),
+                  actions: <Widget>[
+                    FlatButton(
+                        onPressed: () {
+                          Clipboard.setData(ClipboardData(text: '1608889789'));
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('复制')),
+                  ],
+                ));
+          },
+          child: Image.asset(
+            qq,
+            height: 20.0,
+            width: 20.0,
+          ),
+        ),
+        SizedBox(width: 16.0),
+        GestureDetector(
+          onTap: () {
+            launch("https://www.jianshu.com/u/77699cd41b28");
+          },
+          child: Image.asset(
+            jianshu,
+            height: 20.0,
+            width: 20.0,
+          ),
+        ),
+        SizedBox(width: 16.0),
+        GestureDetector(
+          onTap: () {
+            launch("mailto:zzy0523@gmail.com");
+          },
+          child: Image.asset(
+            gmail,
+            height: 20.0,
+            width: 20.0,
+          ),
+        ),
+      ],
+    );
+  }
+
+
 }
