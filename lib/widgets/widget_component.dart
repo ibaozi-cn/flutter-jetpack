e9b566ad716994e56d273b122648dfcbc44db666
zzy <zhangzhanyong@julive.com>
2020-05-13 01:14:52 +0800

    【update theme】

--- lib/widgets/widget_component.dart
+++ lib/widgets/widget_component.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jetpack/data/components.dart';
+import 'package:jetpack/data/const.dart';
 import 'package:jetpack/pages/component_detail.dart';
 import 'package:jetpack/styles/fonts.dart';
 import 'package:jetpack/styles/sizes.dart';
@@ -21,70 +22,135 @@ class _WidgetComponentState extends State<WidgetComponents> {
     return _buildCard(widget.components);
   }
 
-  _buildCard(Components components) {
+//
+//  _buildCard(Components components) {
+//    return Card(
+//      child: InkWell(
+//        onTap: () async {
+//          Navigator.push(context, MaterialPageRoute(builder: (_) {
+//            return PageComponentsDetail(
+//              components: components,
+//              index: 0,
+//            );
+//          }));
+//        },
+//        child: Container(
+//          padding: EdgeInsets.all(16),
+//          child: Column(
+//            mainAxisSize: MainAxisSize.min,
+//            crossAxisAlignment: CrossAxisAlignment.start,
+//            children: <Widget>[
+//              textCardTitle(components.title),
+//              heightBoxMid,
+//              textCardSubTitle(components.subTitle),
+//              heightBoxMid,
+//              Expanded(
+//                child: Row(
+//                  mainAxisAlignment: MainAxisAlignment.end,
+//                  crossAxisAlignment: CrossAxisAlignment.end,
+//                  children: <Widget>[
+//                    InkWell(
+//                      onTap: () {
+//                        Navigator.push(context, MaterialPageRoute(builder: (_) {
+//                          return PageComponentsDetail(
+//                            components: components,
+//                            index: 0,
+//                          );
+//                        }));
+//                      },
+//                      child: textCardActionTitle("示例"),
+//                    ),
+//                    Text('  '),
+//                    InkWell(
+//                      onTap: () {
+//                        _launchURL(components.url);
+//                      },
+//                      child: textCardActionTitle("源码"),
+//                    ),
+//                    Text('  '),
+//                    InkWell(
+//                      onTap: () {
+//                        Navigator.push(context, MaterialPageRoute(builder: (_) {
+//                          return PageComponentsDetail(
+//                            components: components,
+//                            index: 1,
+//                          );
+//                        }));
+//                      },
+//                      child: textCardActionTitle("博客"),
+//                    ),
+//                  ],
+//                ),
+//              )
+//            ],
+//          ),
+//        ),
+//      ),
+//    );
+//  }
+
+  Widget _buildCard(Components components) {
     return Card(
-      child: InkWell(
-        onTap: () async {
-          Navigator.push(context, MaterialPageRoute(builder: (_) {
-            return PageComponentsDetail(
-              components: components,
-              index: 0,
-            );
-          }));
-        },
-        child: Container(
-          padding: EdgeInsets.all(16),
+        child: InkWell(
+      onTap: () async {
+        Navigator.push(context, MaterialPageRoute(builder: (_) {
+          return PageComponentsDetail(
+            components: components,
+            index: 0,
+          );
+        }));
+      },
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: GridTile(
           child: Column(
-            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               textCardTitle(components.title),
               heightBoxMid,
               textCardSubTitle(components.subTitle),
-              heightBoxMid,
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    InkWell(
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (_) {
-                          return PageComponentsDetail(
-                            components: components,
-                            index: 0,
-                          );
-                        }));
-                      },
-                      child: textCardActionTitle("示例"),
-                    ),
-                    Text('  '),
-                    InkWell(
-                      onTap: () {
-                        _launchURL(components.url);
-                      },
-                      child: textCardActionTitle("源码"),
-                    ),
-                    Text('  '),
-                    InkWell(
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (_) {
-                          return PageComponentsDetail(
-                            components: components,
-                            index: 1,
-                          );
-                        }));
-                      },
-                      child: textCardActionTitle("博客"),
-                    ),
-                  ],
-                ),
-              )
+            ],
+          ),
+          footer: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              InkWell(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (_) {
+                    return PageComponentsDetail(
+                      components: components,
+                      index: 0,
+                    );
+                  }));
+                },
+                child: textCardActionTitle("示例"),
+              ),
+              Text('  '),
+              InkWell(
+                onTap: () {
+                  _launchURL(components.url);
+                },
+                child: textCardActionTitle("源码"),
+              ),
+              Text('  '),
+              InkWell(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (_) {
+                    return PageComponentsDetail(
+                      components: components,
+                      index: 1,
+                    );
+                  }));
+                },
+                child: textCardActionTitle("博客"),
+              ),
             ],
           ),
         ),
       ),
-    );
+    ));
   }
 
   _launchURL(url) async {
