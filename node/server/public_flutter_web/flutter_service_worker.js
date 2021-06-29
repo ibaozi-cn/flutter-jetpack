a3e6ade0b33b3927c526f5c967890d7a3c40ee12
zzy <zzy0523@gmail.com>
2021-06-29 22:42:21 +0800

    【更新网站】

--- node/server/public_flutter_web/flutter_service_worker.js
+++ node/server/public_flutter_web/flutter_service_worker.js
@@ -4,18 +4,18 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "281f2a7b1646c3dea6feea25f04fa556",
-"index.html": "70ea039ac2124005983f82a657e9d3d4",
-"/": "70ea039ac2124005983f82a657e9d3d4",
-"main.dart.js": "19d7266af97955c03438da31717efab4",
+"index.html": "5ff45f77b7298edb106a5d4c8b5a2a65",
+"/": "5ff45f77b7298edb106a5d4c8b5a2a65",
+"main.dart.js": "332bcb94bc798ea1a5cb80528d10fe04",
 "favicon.png": "53194b4103559d53e155637d8db1291d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "0823aa3437c7021aa0c3c10faf4668ab",
 "assets/AssetManifest.json": "4d1f4236d216f4fc493049fe67852f99",
-"assets/NOTICES": "88b5dc2967906114a94551d7de08e139",
+"assets/NOTICES": "b1041ac1f1231b13ddcc7fee603e9dde",
 "assets/FontManifest.json": "277df259a6b4de0c68481e262c8a9b95",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/assets/images/head-zzy.png": "aadffbb428561d8ffe27deee778c4294",
 "assets/assets/images/head-sky.png": "a5979121fa6c8ce9add0ae85d3285a7e",
 "assets/assets/images/head-weiqi.png": "a7d6ad250dea8292c1777cfc4a215b1d",
@@ -57,7 +57,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
