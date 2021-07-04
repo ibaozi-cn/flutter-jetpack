acb8df81b48b6220dd8ef89aee3575e5687eb004
ZhangZhanyong <zhangzhanyong@didiglobal.com>
2021-07-04 20:27:38 +0800

    【网站升级改造】

---
+++ node/server/public_flutter_web/precache-manifest.94b7d1fa0c900547483bf2c85b1297fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ece1c77c0dba8b0bce935ed36d4fafa",
+    "url": "/react/riyaqas/index.html"
+  },
+  {
+    "revision": "0bb8c7b74d03696ae5da",
+    "url": "/react/riyaqas/static/js/2.91645c47.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react/riyaqas/static/js/2.91645c47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11d2b3df12647bf2e037",
+    "url": "/react/riyaqas/static/js/main.52e709c5.chunk.js"
+  },
+  {
+    "revision": "d58eb74b9486955d1a52",
+    "url": "/react/riyaqas/static/js/runtime-main.1636be58.js"
+  }
+]);
